ce596d9ea6533774822ee94d68fe1303eb90eca7
Désiré <github.freebie042@passmail.net>
2024-01-29 11:28:10 +0100

    build(back): connect postgres db

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,6 +1,30 @@
 import express from "express";
+import { Pool } from "pg";
+
 const app = express();
 const port = 3000;
+
+// PostgreSQL configuration
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "passworddb",
+  port: 5432,
+});
+
+// Test the database connection
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+    console.error("Error connecting to the database", err);
+  } else {
+    console.log(
+      "Connected to the database. Current time: ",
+      result.rows[0].now
+    );
+  }
+  pool.end();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
