2a44cf18dd73bb6b91c9ae1c99db62d97c0c230a
Désiré <github.freebie042@passmail.net>
2024-01-29 15:14:22 +0100

    build(back): cybertruck object created example

--- back/src/classes.ts
+++ back/src/classes.ts
@@ -2,107 +2,163 @@
 
 //Users class
 class Users {
-    Id: number;
-    Firstname: string;
-    Lastname: string;
-    Email: string;
-    Password: string;
-  
-    constructor() {
-      this.Id = 0;
-      this.Firstname = "";
-      this.Lastname = "";
-      this.Email = "";
-      this.Password = "";
-    }
+  Id: number;
+  Firstname: string;
+  Lastname: string;
+  Email: string;
+  Password: string;
+
+  constructor() {
+    this.Id = 0;
+    this.Firstname = "";
+    this.Lastname = "";
+    this.Email = "";
+    this.Password = "";
   }
-  
-  //Command class
-  class Command {
-    Id: number;
-    IsValid: Boolean;
-    PlaceDelivery: string;
-    Status: string;
-  
-    constructor() {
-      this.Id = 0;
-      this.IsValid = true;
-      this.PlaceDelivery = "";
-      this.Status = "";
-    }
+}
+
+//Command class
+class Command {
+  Id: number;
+  IsValid: Boolean;
+  PlaceDelivery: string;
+  Status: string;
+
+  constructor() {
+    this.Id = 0;
+    this.IsValid = true;
+    this.PlaceDelivery = "";
+    this.Status = "";
   }
-  
-  //--- Factory ---
-  
-  //Factory class
-  abstract class Factory {
-    public abstract factoryMethod(): Car;
-  
-    Id: number = 0;
-    Name: string = "";
-    Engine: string = "";
-    Color: string = "";
+}
+
+//--- Factory ---
+
+//Factory class
+abstract class Factory {
+  public abstract factoryMethod(): Car;
+
+  Id: number = 0;
+  Name: string = "";
+  Engine: number = 0;
+  Color: string = "";
+}
+
+//--- Creators classes ---
+
+//CybertruckCreator class
+class CybertruckCreator extends Factory {
+  public factoryMethod(): Car {
+    return new CybertruckConcrete();
   }
-  
-  //--- Creators classes ---
-  
-  //CybertruckCreator class
-  class CybertruckCreator extends Factory {
-    public factoryMethod(): Car {
-      return new CybertruckConcrete();
-    }
+}
+
+//ModelSCreator class
+class ModelSCreator extends Factory {
+  public factoryMethod(): Car {
+    return new ModelSConcrete();
   }
-  
-  //ModelSCreator class
-  class ModelSCreator extends Factory {
-    public factoryMethod(): Car {
-      return new ModelSConcrete();
-    }
+}
+
+//ModelXCreator class
+class ModelXCreator extends Factory {
+  public factoryMethod(): Car {
+    return new ModelXConcrete();
   }
-  
-  //ModelXCreator class
-  class ModelXCreator extends Factory {
-    public factoryMethod(): Car {
-      return new ModelXConcrete();
-    }
+}
+
+//--- Interface ---
+
+//Car class
+interface Car {
+  Id: number;
+  Name: string;
+  Engine: number;
+  Color: string;
+  getDetails(): string;
+}
+
+//--- Concrete Products ---
+
+//Concrete product Cybertruck
+class CybertruckConcrete implements Car {
+  Id: number;
+  Name: string;
+  Engine: number = 0;
+  Color: string;
+  getDetails() {
+    // Implement getDetails logic for Cybertruck
+    return (
+      "Cybertruck details: " +
+      " id: " +
+      this.Id +
+      " name: " +
+      this.Name +
+      " engine: " +
+      this.Engine +
+      " color: " +
+      this.Color
+    );
   }
-  
-  //--- Interface ---
-  
-  //Car class
-  interface Car {
-    getDetails(): string;
+}
+
+//Concrete product MedelS
+class ModelSConcrete implements Car {
+  Id: number;
+  Name: string;
+  Engine: number = 0;
+  Color: string;
+  getDetails() {
+    // Implement getDetails logic for Cybertruck
+    return (
+      "Cybertruck details: " +
+      " id: " +
+      this.Id +
+      " name: " +
+      this.Name +
+      " engine: " +
+      this.Engine +
+      " color: " +
+      this.Color
+    );
   }
-  
-  //--- Concrete Products ---
-  
-  //Concrete product Cybertruck
-  class CybertruckConcrete implements Car {
-    getDetails() {
-      // Implement getDetails logic for Cybertruck
-      return "Cybertruck details";
-    }
+}
+
+//Concrete product ModelX
+class ModelXConcrete implements Car {
+  Id: number;
+  Name: string;
+  Engine: number = 0;
+  Color: string;
+  getDetails() {
+    // Implement getDetails logic for Cybertruck
+    return (
+      "Cybertruck details: " +
+      " id: " +
+      this.Id +
+      " name: " +
+      this.Name +
+      " engine: " +
+      this.Engine +
+      " color: " +
+      this.Color
+    );
   }
-  
-  //Concrete product MedelS
-  class ModelSConcrete implements Car {
-    getDetails() {
-      // Implement getDetails logic for Cybertruck
-      return "ModelS details";
-    }
-  }
-  
-  //Concrete product ModelX
-  class ModelXConcrete implements Car {
-    getDetails() {
-      // Implement getDetails logic for Cybertruck
-      return "ModelX details";
-    }
-  }
-  
-  //--- Decorator---
-  
-  //Notifier class
-  class Notifier {
-    Notification: string = "";
-  }
+}
+
+//--- Decorator---
+
+//Notifier class
+class Notifier {
+  Notification: string = "";
+}
+
+// Object Cybertruck created example
+const cybertruckCreator = new CybertruckCreator();
+const myCybertruck = cybertruckCreator.factoryMethod();
+myCybertruck.Id = 1;
+myCybertruck.Name = "Cybertruck";
+myCybertruck.Engine = 200;
+myCybertruck.Color = "Grey";
+
+console.log(myCybertruck.getDetails());
