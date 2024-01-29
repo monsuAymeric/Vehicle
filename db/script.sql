37c59ac8cb1fa23f3189c7f772b806cd5a08486f
Désiré <github.freebie042@passmail.net>
2024-01-29 09:39:19 +0100

    build(db): Create script.sql

---
+++ db/script.sql
@@ -0,0 +1,77 @@
+CREATE TABLE
+    Users (
+        idUser SERIAL,
+        firstname VARCHAR(25) NOT NULL,
+        lastname VARCHAR(25) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        password VARCHAR(30) NOT NULL,
+        PRIMARY KEY (idUser),
+        UNIQUE (email)
+    );
+
+CREATE TABLE
+    Colors (
+        idColor SERIAL,
+        name VARCHAR(10) NOT NULL,
+        PRIMARY KEY (idColor)
+    );
+
+CREATE TABLE
+    Engine (
+        idEngine SERIAL,
+        horsePower INTEGER NOT NULL,
+        PRIMARY KEY (idEngine)
+    );
+
+CREATE TABLE
+    Models (
+        idModel SERIAL,
+        name VARCHAR(20) NOT NULL,
+        PRIMARY KEY (idModel)
+    );
+
+CREATE TABLE
+    Adress (
+        idAdress SERIAL,
+        name VARCHAR(20) NOT NULL,
+        PRIMARY KEY (idAdress)
+    );
+
+CREATE TABLE
+    Cars (
+        idCar SERIAL,
+        idColor INTEGER NOT NULL,
+        idModel INTEGER NOT NULL,
+        idAdress INTEGER NOT NULL,
+        idEngine INTEGER NOT NULL,
+        idPrice FLOAT NOT NULL,
+        PRIMARY KEY (idCar),
+        FOREIGN KEY (idColor) REFERENCES Colors (idColor),
+        FOREIGN KEY (idModel) REFERENCES Models (idModel),
+        FOREIGN KEY (idAdress) REFERENCES Adress (idAdress),
+        FOREIGN KEY (idEngine) REFERENCES Engine (idEngine)
+    );
+
+CREATE TABLE
+    Command (
+        idCommand SERIAL,
+        isValid BOOLEAN NOT NULL,
+        placeDelivery VARCHAR(50) NOT NULL,
+        status VARCHAR(20) NOT NULL,
+        idCar INTEGER NOT NULL,
+        idUser INTEGER NOT NULL,
+        idColor INTEGER NOT NULL,
+        idModel INTEGER NOT NULL,
+        idAdress INTEGER NOT NULL,
+        idEngine INTEGER NOT NULL,
+        idPrice FLOAT NOT NULL,
+        PRIMARY KEY (idCommand),
+        UNIQUE (idCar),
+        FOREIGN KEY (idCar) REFERENCES Cars (idCar),
+        FOREIGN KEY (idUser) REFERENCES Users (idUser),
+        FOREIGN KEY (idColor) REFERENCES Cars (idColor),
+        FOREIGN KEY (idModel) REFERENCES Cars (idModel),
+        FOREIGN KEY (idAdress) REFERENCES Cars (idAdress),
+        FOREIGN KEY (idEngine) REFERENCES Cars (idEngine),
+        FOREIGN KEY (idPrice) REFERENCES Cars (idPrice)
+    );
