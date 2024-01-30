ef940a15626317a12d24d888f1a7252748601e2e
Désiré <github.freebie042@passmail.net>
2024-01-30 09:46:12 +0100

    build(front): Build page user

--- front/src/pages/User.jsx
+++ front/src/pages/User.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./User.css";
+import commands from "../datas/Command.json";
 
 export default function User() {
   return (
     <main>
-      <div>User</div>
+      <section>
+        <h1>User page:</h1>
+        <div className="user__container">
+          <div className="user__infos">
+            <b>First name:</b>
+            <p>Elon</p>
+          </div>
+          <div className="user__infos">
+            <b>Last name:</b>
+            <p>Musk</p>
+          </div>
+          <div className="user__infos">
+            <b>Email:</b>
+            <p>elonmusk@tesla.com</p>
+          </div>
+        </div>
+        <div className="user__commands">
+          <h2>Commande(s):</h2>
+          <ul>
+            {commands.map((command) => (
+              <li key={command.id}>{command.name}</li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
