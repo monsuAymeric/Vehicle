95250cf2f7b9c8cb82f83798ac57e35a56de6c61
Désiré <github.freebie042@passmail.net>
2024-01-30 10:51:43 +0100

    build(signup): Start the signup page, form components created

--- front/src/pages/SignUp.jsx
+++ front/src/pages/SignUp.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import "./SignUp.css";
+import Form from "../components/Form";
 
 export default function SignUp() {
+  const inputRef = useRef(null);
+  const handleSubmitButton = () => {
+    alert(inputRef.current.value);
+  };
   return (
     <main>
-      <div>Sign Up</div>
+      <section className="signup__section">
+        <div className="already__signup">
+          <h1>Sign Up</h1>
+          <div className="redirection__sign">
+            <p>Do you already have an account?</p>
+            <Link className="link" to="/signin">
+              Sign In
+            </Link>
+          </div>
+        </div>
+        <Form />
+      </section>
     </main>
-  )
+  );
 }
